9910d1efb42f37fe6833c69eef6408b472f2a56a
Numbers2Crunch <32814807+Numbers2Crunch@users.noreply.github.com>
2017-10-15 14:41:57 +0100

    Hypergeometry in Scipy

--- hypergeom.py
+++ hypergeom.py
@@ -9,7 +9,7 @@ turning up to work drunk depending on the number of tests during the year.
 The number of working days is assumed to be 260 and that an employee
 will turn up drunk 12 times
 
-See code on Scipy for full documentation:
+See Scipy for full documentation and another example:
 https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.hypergeom.html
 
 """
@@ -27,25 +27,19 @@ for testtype, tests in No_of_tests.items():
     # CDF/PMF calcs
     x = np.arange(0, No_of_potential_times_drunk+1)
     
-    # Cummulative Density function - shows chances of getting away if 
-    # the rules change e.g. if we are caught once we might just get a 
+    # Cummulative Density function - shows chances of not getting caught if 
+    # the rules change e.g. if they are caught once they  might just get a 
     # warning.
     cdf_drunk = hypergeom.cdf(x, No_of_Working_days, 
                               No_of_potential_times_drunk, tests)
     
     # Probability Mass function - the probability of turning up
-    # a certain number of times drunk when the company is testing
+    # a certain number of times drunk on test days
     pmf_drunk = hypergeom.pmf(x, No_of_Working_days, 
                               No_of_potential_times_drunk, tests)
-    
-    # This is the inverse of the cdf as a percentile but gives slightly different results
-    ppf_drunk = hypergeom.pmf(x, No_of_Working_days, 
-                              No_of_potential_times_drunk, tests)
-    
+        
     # Set up the annotation to display on the graph
-    # graph_text = 'Prob of being Caught = ' + str(ppf_drunk[0] * 100) + '%' 
     graph_text = 'Prob of being caught = ' + str(round((1 - cdf_drunk[0]),4) * 100) + '%'
-    
     
     # Print the figure
     fig = plt.figure(figsize=(8,8))   
