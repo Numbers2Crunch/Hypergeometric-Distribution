b1ae079351099e823af3b6c1e77635126809198a
Numbers2Crunch <32814807+Numbers2Crunch@users.noreply.github.com>
2017-10-15 14:08:42 +0100

    Hypergeom in Scipy

--- hypergeom.py
+++ hypergeom.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 """
-The problem is as follows: we want to know how likely we are to be caught
-turning up to work drunk given that there will be 52 tests during the 
-working year (260 days) during the year we willturn up drunk 12 times.
+The problem is: we want to know how likely we are to catch an employee
+turning up to work drunk depending on the number of tests during the year.
+The number of working days is assumed to be 260 and that an employee
+will turn up drunk 12 times
 
-See code on Scipy:
+See code on Scipy for full documentation:
 https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.hypergeom.html
 
 """
@@ -24,7 +25,6 @@ for testtype, tests in No_of_tests.items():
     
     # This is the number of arrivals - this will provide a value for 0 in
     # CDF/PMF calcs
-    # i.e. never turning up drunk
     x = np.arange(0, No_of_potential_times_drunk+1)
     
     # Cummulative Density function - shows chances of getting away if 
@@ -34,7 +34,7 @@ for testtype, tests in No_of_tests.items():
                               No_of_potential_times_drunk, tests)
     
     # Probability Mass function - the probability of turning up
-    # so many times drunk
+    # a certain number of times drunk when the company is testing
     pmf_drunk = hypergeom.pmf(x, No_of_Working_days, 
                               No_of_potential_times_drunk, tests)
     
@@ -43,9 +43,8 @@ for testtype, tests in No_of_tests.items():
                               No_of_potential_times_drunk, tests)
     
     # Set up the annotation to display on the graph
-    # graph_text = 'Prob of being Caught = ' + str(ppf_drunk[0] * 100) + '%'
-    
-    graph_text = 'Prob of being Caught = ' + str(round((1 - cdf_drunk[0]),4) * 100) + '%'
+    # graph_text = 'Prob of being Caught = ' + str(ppf_drunk[0] * 100) + '%' 
+    graph_text = 'Prob of being caught = ' + str(round((1 - cdf_drunk[0]),4) * 100) + '%'
     
     
     # Print the figure
@@ -55,10 +54,10 @@ for testtype, tests in No_of_tests.items():
     ax = plt.subplot(211)
     plt.annotate(graph_text, xy=(0 + 0.2, cdf_drunk[0] + 0.01), xytext=(2, 0.4),
                 arrowprops=dict(facecolor='red', shrink=0.01),)
+    
     ax.plot(x, cdf_drunk, marker = '.', mfc = 'red')
-    
     ax.set_ylabel('Hypergeometric CDF and PMF')
-    
+    ax.set_xlabel('No of drunk of Arrivals')
     ax.plot(x, pmf_drunk, 'bo')
     ax.vlines(x, 0, pmf_drunk, lw=1)
     
